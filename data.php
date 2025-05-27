a474507a859052154f9825576a83c10db72c0444
lim091017 <jasonlimzishen09@gmail.com>
2025-05-27 12:12:23 +0800

    Add files via upload

---
+++ data.php
@@ -0,0 +1,31 @@
+<?php
+    $servername = "localhost
+    $username = "root";
+    $password = "";
+    $database = "sneakerstore";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn->connect_error) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $name = $+POST['name'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $card_number = $_POST['card_number'];
+    $expiry_month = $_POST['expiry_month'];
+    $expiry_year = $_POST['expiry_year'];
+    $cvv = $_POST['cvv'];
+
+    $sql = "INSERT INTO orders (name, phone, address, card_number, expiry_month, expiry_year, cvv) 
+    VALUES ('$name', '$phone', '$address', '$card_number', '$expiry_month', '$expiry_year', '$cvv')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('Checkout complete');</script>";
+
+        echo "<script>window.setTimeout(function(){ window.location.href = 'index.html'; }, 1000);</script>";
+    } else {
+        echo "Error : "" .$sql . "<br>" . $conn->error;
+    }
+?>
